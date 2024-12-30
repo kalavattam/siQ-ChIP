dc2d2b0c0ce3a07b8564067bc0203a3e4b903cf8
Kris Alavattam <34422552+kalavattam@users.noreply.github.com>
2024-12-30 13:13:00 -0500

    Add updated (commented) tracks.f90 and runCrunch.sh, as well as notes for tracks.f90

--- runCrunch.sh
+++ runCrunch.sh
@@ -1,46 +1,71 @@
+#  Set the bin width for processing (used for normalization and calculations later).
 widths=30
-narg=`echo $@ | wc |awk '{print $2}'`
-if [ $narg -lt 4 ] ; then
-echo 'Run this scrtip as:
-./runCrunch.sh IPFILE INPUTFILE PARAMfile TAG
 
-The path and name of IPFILE and INPUTFILE must be less than 65 characters
-PARAMfile are the siqchip measurements
-TAG is the name your final SIQ-ChIP file will have
-Currently you are missing one of these arguments. <----
-'
+#  Count the number of arguments passed to the script.
+narg=`echo $@ | wc | awk '{ print $2 }'`
+
+#  Check if the required number of arguments (4) is provided.
+if [ ${narg} -lt 4 ]; then
+    # Display usage instructions if fewer than 4 arguments are provided.
+    echo 'Run this script as:
+    ./runCrunch.sh IPFILE INPUTFILE PARAMfile TAG
+
+    IPFILE: Path to the main (IP) data file.
+    INPUTFILE: Path to the control (input) data file.
+    PARAMfile: Path to the parameter file for siQ-ChIP measurements.
+    TAG: A custom name for the output siQ-ChIP file.
+
+    Note: The path and name of IPFILE and INPUTFILE must each be less than 65 characters.
+
+    Currently, you are missing one or more of these arguments. <----
+    '
 else 
-ipfile=`echo $1`
-infile=`echo $2`
-paramfile=`echo $3`
-tag=`echo $4`
-nip=`wc -l $ipfile |awk '{print $1}'`
-nin=`wc -l $infile |awk '{print $1}'`
-legs=`awk '{sum+=$4} END {print sum/NR}' $infile`
+    #  Assign arguments to variables for clarity and later usage
+    ipfile=${1}     # Path to the IP file
+    infile=${2}     # Path to the input (control) file
+    paramfile=${3}  # Path to the parameter file
+    tag=${4}        # Custom name for the output file
 
-dep=`echo $nin*$widths/3200000000./\(1-$widths/3200000000\) |bc -l` #average layer on input
-echo $dep
+    #  Calculate the number of lines (regions) in the IP and input files
+    nip=`wc -l ${ipfile} | awk '{ print $1 }'`  # Number of lines in IP file
+    nin=`wc -l ${infile} | awk '{ print $1 }'`  # Number of lines in input (control) file
 
-#skip bug
-gfortran -O3 -fbounds-check tracks.f90
-./a.out $ipfile $infile $widths $widths
+    #  Compute the average region length from the input (control) file (4th
+    #+ column values; currently unused)
+    legs=`awk '{ sum += $4 } END { print sum/NR }' ${infile}`
 
+    #  Compute the depth factor for input normalization
+    dep=`echo $nin*$widths/3200000000./$begin:math:text$1-$widths/3200000000$end:math:text$ | bc -l` 
+    echo ${dep}  # Print the computed depth factor for reference
 
-#get alpha
-gfortran -O3 -fbounds-check getalpha.f90
-a=`./a.out $paramfile $nin $nip`
-echo "i called alpha already" $nin $nip $a
-echo $a $nin $nip > $tag.alpha
-#skip bug
-echo $dep " is dep"
-gfortran -O3 -fbounds-check mergetracks.f90
-./a.out IP.data IN.data $a $dep 
-mv mergedSIQ.data $tag.bed
-echo "you created the file: " $tag.bed
+    #  Compile the `tracks.f90` program and execute it; `tracks.f90` processes
+    #+ the IP and input files, generating binned coverage files
+    gfortran -O3 -fbounds-check tracks.f90            # Compile with optimizations and bounds checking
+    ./a.out ${ipfile} ${infile} ${widths} ${widths}   # Execute with input parameters
 
-nl=`wc -l $ipfile |awk '{print $1}'`
-awk -v var=$nl '{print $1,$2,$3,$4/var}' IP.data > NormCovIP_$tag.bed
-nl=`wc -l $infile |awk '{print $1}'`
-awk -v var=$nl '{print $1,$2,$3,$4/var}' IN.data > NormCovIN_$tag.bed
+    #  Compile and run `getalpha.f90` to compute the alpha scaling factor
+    gfortran -O3 -fbounds-check getalpha.f90
+    a=`./a.out ${paramfile} ${nin} ${nip}`            # Capture alpha value output from 'getalpha.f90'
+    echo "i called alpha already" ${nin} ${nip} ${a}  # Print debug message for alpha calculation
+    echo ${a} ${nin} ${nip} > $tag.alpha              # Save alpha and file line counts to a tag-specific file
 
+    #  Print the depth factor again for reference
+    echo "${dep} is dep"
+
+    #  Compile and run 'mergetracks.f90' to combine the IP and input data using the alpha and depth factors
+    gfortran -O3 -fbounds-check mergetracks.f90
+    ./a.out IP.data IN.data ${a} ${dep}  # Pass the processed data files and scaling factors
+    
+    #  Rename the merged output file to a tag-specific name with a '.bed' extension
+    mv mergedSIQ.data ${tag}.bed
+    echo "you created the file: " ${tag}.bed  # Confirm output creation
+
+    #  Compute normalized coverage for IP and input files 
+    nl=`wc -l ${ipfile} | awk '{ print $1 }'`  # Count lines in the IP file
+    awk -v var=${nl} '{ print $1,$2,$3,$4 }' IP.data > IntermedIP_${tag}.bed  # Non-normalized intermediate IP coverage
+    awk -v var=${nl} '{ print $1,$2,$3,$4/var }' IP.data > NormCovIP_${tag}.bed   # Normalize IP coverage
+
+    nl=`wc -l ${infile} | awk '{ print $1 }'`  # Count lines in the input file
+    awk -v var=${nl} '{ print $1,$2,$3,$4 }' IN.data > IntermedIN_${tag}.bed  # Non-normalized intermediate input coverage
+    awk -v var=${nl} '{ print $1,$2,$3,$4/var }' IN.data > NormCovIN_${tag}.bed   # Normalize input coverage and save
 fi
--- tracks.f90
+++ tracks.f90
@@ -1,198 +1,156 @@
-      real*8 bin(100000),sum,itops
-!      integer bin(100000),nleng(2),ilft, irght, leng,leftend,rightend,Reason,sum,lpos
-      integer nleng(2),ilft, irght, leng,leftend,rightend,Reason,lpos
-      character(len=6) :: unk,rchr
-      character(len=5) :: unk2,rchr2
-      character(len=44) :: siq!for hmm annos
-      character(len=62) :: arg
-      character(len=62) :: path(2)
-      logical :: file_exists
-      inot=0
-      itops=0
-      iread=0
-      rchr="chr1" !starts on chr1
-      do i=1,iargc()
-         if(i.lt.3)then
-         call getarg(i, arg)
-            path(i)=arg
-         else
-         call getarg(i, arg)
-            read(arg,*)nleng(i-2)
-         endif
-      enddo
-      do k=1,2 !files loop
-      inquire(file=path(k),EXIST=file_exists)
-      if(file_exists .eqv. .true.)then
-         open(12,file=path(k))      
-      else
-         write(*,*) 'your first file or path is incorrect'
-         stop
-      endif
+! Declare variables
+real*8 bin(100000), sum, itops  ! Array for binning data, 'bin'; scalars for cumulative sum, 'sum', and max bin value, 'itops'
+! Declare integer variables:
+! `nleng(2)`: Array for bin sizes of the two input files.
+! `ilft`, `irght`: Start and end positions of the genomic region being processed.
+! `leng`: Length of the region (`irght - ilft`).
+! `leftend`, `rightend`: Boundaries of the current binning range.
+! `Reason`: I/O status code after reading input.
+! `lpos`: Tracks current genomic position in the `bin` array.
+integer nleng(2), ilft, irght, leng, leftend, rightend, Reason, lpos
+character(len=6) :: unk, rchr      ! Chromosome identifiers
+character(len=5) :: unk2, rchr2    ! Alternate chromosome identifiers (unused)
+character(len=44) :: siq           ! Placeholder for HMM annotations (unused)
+character(len=62) :: arg, path(2)  ! Infile paths
+logical :: file_exists             ! File existence flag
+inot = 0       ! Integer variable initialized to 0 (unused)
+itops = 0      ! Tracks maximum value in bin array
+iread = 0      ! Integer variable initialized to 0 (unused)
+rchr = "chr1"  ! Start processing from chromosome "chr1"
 
-      bin=0d0
-      leftend=0
-      rightend=0
+! Read command-line arguments to get file paths and bin sizes
+do i = 1, iargc()              ! Loop through all CL arguments
+   if (i.lt.3) then            ! First two arguments are infile paths
+      call getarg(i, arg)      ! Get the argument string
+      path(i) = arg            ! Store file path
+   else                        ! Third argument onwards specifies bin sizes
+      call getarg(i, arg)      ! Get argument string
+      read(arg, *) nleng(i-2)  ! Read and store bin size for each input file
+   endif
+enddo
 
-      if(k.eq.1)open(88,file='IP.data')
-      if(k.eq.2)open(88,file='IN.data')
-      if(k.eq.2)open(89,file='INave.data')
-12345 continue
-      read(12,*,IOSTAT=Reason) unk, ilft, irght, leng      
-      if(Reason.gt.0)then
-         write(*,*) 'there was an error in input file ', path(k)
-         stop
-      elseif(Reason.eq.0)then 
-23456    continue
-!only work the given chr by using this line
-!      if(unk /= "chr1")go to 54321 
+! Loop through the two input files
+do k = 1, 2
+   inquire(file=path(k), EXIST=file_exists)  ! Check that file exists
+   if (file_exists) then
+      open(12, file=path(k))                 ! Open file for reading
+   else
+      write(*, *) 'Your first file or path is incorrect'
+      stop                                   ! Terminate program if file is missing
+   endif
 
-      if(SCAN(unk(5:6), "_").eq.0)then
+   bin = 0d0     ! Initialize bin array to zero
+   leftend = 0   ! Reset left boundary of current region
+   rightend = 0  ! Reset right boundary of current region
 
-         if(ilft.gt.rightend.and.unk == rchr)then
-            !write out the current bin of data
-            if(itops.gt.1*0)then !nonempty
-               lpos=leftend
-               ave=0d0
-               count=0d0
-               do i=1,rightend-leftend,nleng(k)
-                  sum=0
-                  iend=min(rightend-leftend,i+nleng(k)-1)!double test
-                  idub=0
-                  do j=i,iend
-                     lpos=lpos+1
-                     idub=idub+1
-                     sum=sum+bin(j)
+   ! Open output files based on input file type
+   if (k.eq.1) open(88, file='IP.data')    ! File for treatment data
+   if (k.eq.2) open(88, file='IN.data')    ! File for control data
+   if (k.eq.2) open(89, file='INave.data') ! File for averaged control data
+
+12345 continue  ! Label: Loop back after processing single input line
+   read(12, *, IOSTAT=Reason) unk, ilft, irght, leng  ! Read input line: chrom, start, end, length
+   if (Reason.gt.0) then
+      write(*, *) 'There was an error in input file ', path(k)  ! Handle I/O errors
+      stop                                                      ! Stop execution if there's an error
+   elseif (Reason.eq.0) then
+23456 continue  ! Label: Begin to process successfully read input line
+      ! Skip regions not on current chromosome
+      if (unk /= rchr) go to 54321  ! If chromosome changes, process current chromosome and switch
+
+      ! Process regions only if chromosome identifier is valid
+      if (SCAN(unk(5:6), "_").eq.0) then  ! Check that 5th and 6th characters of 'unk' don't contain underscore
+         ! Handle new, non-overlapping regions
+         if (ilft.gt.rightend.and.unk == rchr) then           ! If new region starts after current region
+            ! Write out current bin data
+            if (itops.gt.0) then                              ! Execute if current bin has data
+               lpos = leftend                                 ! Initialize position counter to left boundary
+               ave = 0d0                                      ! Reset total average accumulator
+               count = 0d0                                    ! Reset range counter
+               do i = 1, rightend-leftend, nleng(k)           ! Iterate over bins with step size of bin length
+                  sum = 0                                     ! Reset sum for current range
+                  iend = min(rightend-leftend, i+nleng(k)-1)  ! Define end of the range
+                  idub = 0                                    ! Reset bin counter for this range
+                  do j = i, iend                              ! Loop through indices in range
+                     lpos = lpos + 1                          ! Update genomic position
+                     idub = idub + 1                          ! Increment number of bins in range
+                     sum = sum + bin(j)                       ! Add bin value to sum
                   enddo
-!                  write(88,*) unk, lpos-nleng(k)/2, dble(sum)/dble(nleng(k))
-                  write(88,*) unk, lpos-idub,lpos-1, dble(sum)!/dble(idub)
-                  ave=ave+dble(sum)/dble(idub)
-                  count=count+1d0
+                  write(88, *) unk, lpos-idub, lpos-1, dble(sum)  ! Output range data: chromosome, start, end, sum
+                  ave = ave + dble(sum) / dble(idub)  ! Add average value for range to total average
+                  count = count + 1d0                 ! Increment range counter
                enddo
-               if(k.eq.2)then
-                  write(89,*) unk, leftend, rightend, ave/count
+               if (k.eq.2) then
+                  write(89, *) unk, leftend, rightend, ave / count  ! Output average for control data
                endif
             endif
-            bin=0
-            itops=0
-            !set new bins
-            leftend=ilft
-            rightend=ilft+leng!same as irght sometimes
-            do i=1,rightend-leftend
-               bin(i)=bin(i)+1d0/dble(leng)
-               itops=max(itops,bin(i))
+            bin = 0    ! Reset bin array
+            itops = 0  ! Reset maximum bin value
+
+            ! Update boundaries for new region
+            leftend = ilft
+            rightend = ilft + leng
+            do i = 1, rightend-leftend
+               bin(i) = bin(i) + 1d0 / dble(leng)  ! Initialize bins for new region with normalized values
+               itops = max(itops, bin(i))          ! Update maximum bin value
             enddo
-            go to 12345
-         elseif(ilft.le.rightend.and.unk == rchr)then
-            iindicate=0
-            rightend=max(rightend,ilft+leng)!new right end
-!            leftend = 1 in position so ilft-leftend+1 is start location
-!            if(1+ilft-leftend.lt.1)then
-!               write(*,*) unk, ilft, leftend, "ji", rchr
-!               stop
-!            endif
-            if(1+ilft-leftend.lt.100000.and.rightend-leftend.le.100000)then
-            do i=min(100000,1+ilft-leftend),min(100000,1+ilft-leftend+leng)!rightend-leftend
-               bin(i)=bin(i)+1d0/dble(leng)
-               itops=max(itops,bin(i))
-            enddo   
-            go to 12345
-            else!the bounds of bin will be exceeded so reset bins now-can happen on inputs
-               !final update and dump it out
-               do i=min(100000,1+ilft-leftend),min(100000,1+ilft-leftend+leng)!rightend-leftend
-                  bin(i)=bin(i)+1d0/dble(leng)
-                  itops=max(itops,bin(i))
+            go to 12345  ! Return to main processing loop
+         elseif (ilft.le.rightend.and.unk == rchr) then  ! If region overlaps with current one
+            ! Update bins if regions overlap
+            rightend = max(rightend, ilft + leng)        ! Extend right boundary to include new region
+            if (1+ilft-leftend.lt.100000.and.rightend-leftend.le.100000) then        ! Ensure bin indices stay within array bounds (1 to 100000)
+               do i = min(100000, 1+ilft-leftend), min(100000, 1+ilft-leftend+leng)  ! Update bins safely within allowed range for current genomic region
+                  bin(i) = bin(i) + 1d0 / dble(leng)  ! Update overlapping bins with normalized values
+                  itops = max(itops, bin(i))          ! Update maximum bin value
+               enddo
+               go to 12345                            ! Continue processing next region
+            else
+               ! Handle bin overflow if region is too large
+               do i = min(100000, 1+ilft-leftend), min(100000, 1+ilft-leftend+leng)  ! Handle bins that exceed array bounds
+                  bin(i) = bin(i) + 1d0 / dble(leng)  ! Add normalized values to bins
+                  itops = max(itops, bin(i))          ! Update maximum bin value
                enddo
 
-            if(itops.gt.1*0)then!not likely needed
-               lpos=leftend
-               ave=0d0
-               count=0d0
-               do i=1,ilft,nleng(k)!rightend-leftend,nleng(k)
-                  sum=0
-                  idub=0
-                  iend=min(ilft-leftend,i+nleng(k)-1)!double test
-                  do j=min(i,100000),min(iend,100000)!double test
-                     lpos=lpos+1
-                     idub=idub+1
-                     sum=sum+bin(j)
+               ! Write out current bins
+               if (itops.gt.0) then  ! Check that there are data in current bins
+                  lpos = leftend     ! Start genomic position counter at left boundary
+                  ave = 0d0          ! Initialize total average accumulator for region
+                  count = 0d0        ! Initialize counter for number of processed ranges
+                  do i = 1, ilft, nleng(k)  ! Loop through all bins in region in steps of bin size 'nleng(k)'
+                     sum = 0         ! Reset cumulative sum for current range
+                     idub = 0        ! Reset counter for number of bins in range
+                     iend = min(ilft-leftend, i+nleng(k)-1)  ! Determine end index for range, ensuring 'iend' doesn't exceed region bounds ('ilft-leftend') or cause array overflow
+                     
+                     do j = min(i, 100000), min(iend, 100000)  ! Loop through bins in current range
+                        lpos = lpos + 1                        ! Increment genomic position for each bin processed
+                        idub = idub + 1                        ! Increment bin count for range
+                        sum = sum + bin(j)                     ! Add value of current bin to cumulative sum
+                     enddo
+
+                     if (idub.gt.0) then  ! Process the range only if bins are in it
+                        ! Output chromosome, range, and sum for bins in range
+                        write(88, *) unk, lpos-idub, lpos-1, dble(sum)
+                        ave = ave + dble(sum) / dble(idub)  ! Add range's average to total average
+                        count = count + 1d0                 ! Increment processed range counter
+                     endif
                   enddo
-!                  write(88,*) unk, lpos-nleng(k)/2, dble(sum)/dble(nleng(k))
-                  if(idub.gt.1)then
-                  write(88,*) unk, lpos-idub,lpos-1, dble(sum)!/dble(idub)
-                  ave=ave+dble(sum)/dble(idub)
-                  count=count+1d0
+
+                  if (k.eq.2) then  ! Special case: If processing control file (k = 2)
+                     ! Output average signal for entire region to separate file
+                     write(89, *) unk, leftend, rightend, ave / count  ! Write average
                   endif
-               enddo
-               iindicate=1
-               if(k.eq.2)then
-                  write(89,*) unk, leftend, rightend, ave/count
                endif
 
+               bin = 0           ! Reset bin array to prepare for next region
+               itops = 0         ! Reset maximum bin value for next region
+               leftend = ilft    ! Update left boundary for next genomic region
+               rightend = irght  ! Update right boundary for next genomic region
             endif
-            if(iindicate.eq.0)then
-               bin=0
-            itops=0
-            !set new bins
-            leftend=0
-            rightend=0
-            else!ilft should hold the last fragment read and 
-! ----------------- WORKING HERE TO SHIFT BIN correctly
-               itops=0!clear this
-               j=0
-               do i=1+ilft-leftend,100000
-                  j=j+1
-                  bin(j)=bin(i)
-                  itops=max(itops,bin(j))                  
-               enddo!anything outside should have been zero anyway
-               !we need to implement better bookkeeping right here, imho
-               do i=j+1,100000
-                  bin(i)=0d0
-               enddo
-               leftend=ilft
-               rightend=irght!rightend!same one
-            endif
-            rchr=unk
-            go to 12345!23456
-            endif!protect bounds of bin
+            go to 12345  ! Jump back to process next line of input
          endif
-         if(unk /= rchr)then
-            write(*,*) unk, rchr, k, itops
-            if(itops.gt.1*0)then
-               lpos=leftend
-               ave=0d0
-               count=0d0
-               do i=1,rightend-leftend,nleng(k)
-                  sum=0
-                  idub=0
-                  do j=i,min(rightend-leftend,i+nleng(k)-1)!double test
-                     lpos=lpos+1
-                     idub=idub+1
-                     sum=sum+bin(j)
-                  enddo
-                  write(88,*) rchr, lpos-idub,lpos-1, dble(sum)!/dble(idub)
-!                  write(88,*) unk, lpos-nleng(k)/2, dble(sum)/dble(nleng(k))
-                  ave=ave+dble(sum)/dble(idub)
-                  count=count+1d0
-               enddo
-               if(k.eq.2)then
-                  write(89,*) rchr, leftend, rightend, ave/count
-               endif
-            endif
-            bin=0
-            itops=0
-            !set new bins
-            leftend=0
-            rightend=0
-            rchr=unk
-            go to 23456
-         endif
-         else
-            go to 12345
-         endif!the _ screen
       endif
-54321 continue!jumpout landing
-      close(12)
-      close(88)
-      enddo!k files loop
-
-    end program
+54321 continue   ! Label: Mark end of processing for current chromosome
+      close(12)  ! Close infile for current dataset
+      close(88)  ! Close main outfile
+   enddo         ! End loop over the two input files
+end program
